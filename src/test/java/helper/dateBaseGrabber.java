0d6bcb0a079a44968af197d8b5218f59abed4144
m6dusa <makowski.re10ad@gmail.com>
2017-12-01 00:55:35 +0300

    just a small commit

--- src/test/java/helper/dateBaseGrabber.java
+++ src/test/java/helper/dateBaseGrabber.java
@@ -1,23 +1,31 @@
 package helper;
 
 import com.mongodb.*;
-import com.mongodb.client.FindIterable;
+
 import com.mongodb.client.MongoCollection;
+
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 public class dateBaseGrabber {
+
     MongoClientURI address = new MongoClientURI("mongodb://54.88.178.46:27020");
-//    public String getSecretAnswerFor(String email) throws Exception {
-//        try {
-//            MongoClient mc = new MongoClient(address);
-//            MongoDatabase db = mc.getDatabase("classwallet");
-//            MongoCollection<Document> collection = db.getCollection("users");
-//            DBObject query = new BasicDBObject(new Document"profile.email", email);
-//            FindIterable<Document> cursor = collection.find((Bson) query);
-//        }catch(Exception ex) {
-//            throw new Exception();
-//        }finally{}
-//    }
+    public String getSecretAnswerFor(String email) throws Exception {
+        try(MongoClient mc = new MongoClient(address)) {
+
+         MongoDatabase db = mc.getDatabase("classwallet");
+           MongoCollection<Document> collection = db.getCollection("users");
+            Document document = collection
+                    .find(new BasicDBObject("profile.email", "email"))
+                    .projection(Projections.fields(Projections.include("security.answer"), Projections.excludeId())).first();
+            return document.get("anwser").toString();
+           //DBObject query = new BasicDBObject(new Document("profile.email", email));
+           //MongoCursor<Document> cursor = collection.find().iterator();
+       }catch(Exception ex) {
+            ex.printStackTrace();
+       }finally{mc.close();}
+}
 }
